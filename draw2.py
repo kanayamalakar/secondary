8b06ea0be57e34fa29074c9c485d2ddd1141306f
Kanaya Malakar <malakarkanaya@gmail.com>
2018-06-05 13:23:38 +0000

    Update draw2.py

--- draw2.py
+++ draw2.py
@@ -1,7 +1,7 @@
 # This code draws a woman's face using basic mathematical curves. eg. parabola for face, sine for hair etc. 
 # Originally written in python2, it works in python3 as well.
 
-# output file: Indianwoman.png
+# output file: face.png
 
 # AUTHOR : Kanaya Malakar
 # COPYRIGHT : Kanaya Malakar 2018
